139b9dbbed5760dcc662b06b8eb578040c67badc
Mikhail Zhukov <M-kh-l.i-ai-O@yandex.ru>
2024-10-20 22:27:00 +0300

    refac

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -6,7 +6,7 @@ from pep_parse.items import PepParseItem
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['https://peps.python.org/',]
+    start_urls = ['https://peps.python.org/', ]
 
     def parse(self, response):
         all_link = response.css('a.pep.reference.internal')
